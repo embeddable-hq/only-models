5b83f5838925ab9aa75b556b1efb258273146dc6
Tom Gardiner <tom@trevor.io>
2025-03-13 16:41:06 +0100

    remove unnecessary react plugin

--- embeddable.config.js
+++ embeddable.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from '@embeddable.com/sdk-core';
-import react from '@embeddable.com/sdk-react';
 
 export default defineConfig({
-  plugins: [react],
+  plugins: [],
   modelsSrc: './src/models',
   presetsSrc: './src/presets',
   
